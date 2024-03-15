805f4979cb32ab63be8887f0eaace32b4c30bfc3
Carsten Kremser <github@carstenkremser.de>
2024-03-15 11:07:24 +0100

    Initial commit

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Vehicle {
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+
+    public Vehicle(String manufacturer, String model, int yearOfManufacture) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+}
