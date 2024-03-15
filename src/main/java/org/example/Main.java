d19474cd75cd0750de73299c353ca99f0f66d6f8
Carsten Kremser <github@carstenkremser.de>
2024-03-15 11:33:35 +0100

    Add motorcycle in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,10 @@ public class Main {
 
         Vehicle myBike = new Vehicle("Peugeot", "27 Bike", 1990);
         Car myCar = new Car("BMW", "3 series", 2005, 5);
+        Motorcycle myMotorcycle = new Motorcycle("Aprilia", "RS660", 2021,"Sportsbike");
 
         System.out.println(myBike);
         System.out.println(myCar);
+        System.out.println(myMotorcycle);
     }
 }
